d9a7fba54538dd0f6298b580764e6275d51449db
Moajjem Hossain <moajjem441@gmail.com>
2026-05-26 22:10:49 +0600

    register part updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 'use client';
+import { authClient } from '@/lib/auth-client';
+import { Button } from '@heroui/react';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
-    const pathname=usePathname();
+    const pathname = usePathname();
 
+    const {data:session}=authClient.useSession();
+    const user=session?.user;
+
+    //  console.log("Session Data in Navbar:", session);
 
     return (
         <div className="navbar bg-base-100 shadow-sm max-w-11/12 mx-auto rounded-3xl p-5 mt-5">
@@ -16,37 +23,64 @@ export default function Navbar() {
                         tabIndex="-1"
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow z-10">
                         <li>
-                            <a href="/" className={pathname==='/' ? 'bg-cyan-600' : ''}>Home</a>    
+                            <a href="/" className={pathname === '/' ? 'bg-cyan-600' : ''}>Home</a>
                         </li>
                         <li>
-                            <a href='/all-courses' className={pathname==='/all-courses' ? 'bg-cyan-600' : ''}>Courses</a>  
+                            <a href='/all-courses' className={pathname === '/all-courses' ? 'bg-cyan-600' : ''}>Courses</a>
                         </li>
                         <li>
-                            <a href='/my-profile' className={pathname==='/my-profile' ? 'bg-cyan-600' : ''}>My Profile</a>
+                            <a href='/my-profile' className={pathname === '/my-profile' ? 'bg-cyan-600' : ''}>My Profile</a>
                         </li>
-                        
-                         
-                            
+
+
+
                     </ul>
                 </div>
-               <h1 className="animate__animated animate__bounce">SkillSphere</h1>
+                <h1 className="animate__animated animate__bounce">SkillSphere</h1>
                 {/* <a className="btn btn-ghost text-xl">SkillSphere</a> */}
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 ">
                     <li>
-                            <a href="/" className={pathname==='/' ? 'bg-cyan-600' : ''}>Home</a>    
-                        </li>
-                        <li>
-                            <a href='/all-courses' className={pathname==='/all-courses' ? 'bg-cyan-600' : ''}>Courses</a>  
-                        </li>
-                        <li>
-                            <a href='/my-profile' className={pathname==='/my-profile' ? 'bg-cyan-600' : ''}>My Profile</a>
-                        </li>
+                        <a href="/" className={pathname === '/' ? 'bg-cyan-600' : ''}>Home</a>
+                    </li>
+                    <li>
+                        <a href='/all-courses' className={pathname === '/all-courses' ? 'bg-cyan-600' : ''}>Courses</a>
+                    </li>
+                    <li>
+                        <a href='/my-profile' className={pathname === '/my-profile' ? 'bg-cyan-600' : ''}>My Profile</a>
+                    </li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+               <ul className='flex gap-2 items-center'>
+                {
+                    user ?
+                    <>
+                     <li>
+                    <Avatar>
+        <Avatar.Image alt={user?.name} src={user?.image} />
+        <Avatar.Fallback>{user?.name}</Avatar.Fallback>
+      </Avatar>
+                    
+                </li>
+
+                <li>
+                    <a href="/login" ><Button variant="danger">Log Out</Button></a>
+                </li>
+
+                    </>
+
+                    :
+                    <>
+
+                    <li><Link href="/register">Register</Link></li>
+                <li><Link href="/login">Log In</Link></li>
+                    </>
+                }
+            
+                </ul>
+             
             </div>
         </div>
     );
