14f523bad32398556a2cb3998139d76ec7e3a478
Moajjem Hossain <moajjem441@gmail.com>
2026-05-27 13:47:21 +0600

    register part updated

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -13,59 +13,61 @@ const RegisterPage = () => {
         e.preventDefault();
 
         const formData = new FormData(e.currentTarget);
-        const  userData = Object.fromEntries(formData.entries());
+        const userData = Object.fromEntries(formData.entries());
 
         console.log("Form Data :", userData);
 
         // const {name,email,photoURL,password}= data;
 
-        
-           const { data, error } = await authClient.signUp.email({
-    name: userData?.name, 
-    email: userData?.email, 
-    password: userData?.password, 
-    image: userData?.photoURL,
-    // callbackURL: "https://example.com/callback",
-});
 
-        if(error){
+        const { data, error } = await authClient.signUp.email({
+            name: userData?.name,
+            email: userData?.email,
+            password: userData?.password,
+            image: userData?.photoURL,
+            // callbackURL: "https://example.com/callback",
+        });
+
+        if (error) {
             toast.error("Registration failed. Please try again.");
             return;
         }
 
-        if(data){
+        if (data) {
             toast.success("Registration successful! Please login to your account.");
             router.push('/login')
         }
 
-        
-        
+
+
     };
 
 
-   const GoogleSignIn = async () => {
-    try {
-        await authClient.signIn.social({
-            provider: "google",
-            callbackURL: "/",
-        });
+    const GoogleSignIn = async () => {
+        try {
+            await authClient.signIn.social({
+                provider: "google",
+                callbackURL: "/",
+            });
 
-    } catch (error) {
-        console.error(error);
-        toast.error("Google login failed");
-    }
-};
+        } catch (error) {
+            console.error(error);
+            toast.error("Google login failed");
+        }
+    };
 
     return (
         <div className="flex items-center justify-center h-screen">
 
-            <form onSubmit={handleRegisterSubmit}>
 
-                <div className="card bg-base-100 max-w-full shadow-sm p-18 rounded-2xl">
 
-                    <h1 className="text-2xl font-bold mb-6 text-center text-cyan-100">
-                        Register Your Account
-                    </h1>
+            <div className="card bg-base-100 max-w-full shadow-sm p-18 rounded-2xl">
+
+                <h1 className="text-2xl font-bold mb-6 text-center text-cyan-100">
+                    Register Your Account
+                </h1>
+
+                <form onSubmit={handleRegisterSubmit}>
 
                     <div className="mb-4">
                         <label>Name</label>
@@ -118,28 +120,31 @@ const RegisterPage = () => {
                         Register
                     </button>
 
-                    <p className="mt-4 text-center">
-                        Already have an account?{" "}
-                        <Link href="/login" className="text-blue-500">
-                            Login
-                        </Link>
-                    </p>
+
+                </form>
+
+                <p className="mt-4 text-center">
+                    Already have an account?{" "}
+                    <Link href="/login" className="text-blue-500">
+                        Login
+                    </Link>
+                </p>
 
 
-                     <div className="flex items-center justify-center mt-4">
+                <div className="flex items-center justify-center mt-4">
                     <button onClick={GoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
-                <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                Login with Google
-            </button>
+                        <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                        Login with Google
+                    </button>
                 </div>
 
-                </div>
+            </div>
 
-               
 
-            </form>
 
-            
+
+
+
 
         </div>
     );
@@ -169,7 +174,7 @@ export default RegisterPage;
 //             email: userData.email,
 //             password: userData.password,
 //             // এখানে userData.photoURL ব্যবহার করুন, কারণ আপনার ফর্মে নাম photoURL
-//             image: userData.photoURL, 
+//             image: userData.photoURL,
 //         });
 
 //         if (error) {
