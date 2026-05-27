feddc24c3211155b53cda19e1b1507692869c198
Moajjem Hossain <moajjem441@gmail.com>
2026-05-27 13:45:20 +0600

    log in part updated

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,7 +6,24 @@ import toast from 'react-hot-toast';
 
 
 const LogInPage = () => {
-     const router =useRouter();
+    const router = useRouter();
+
+
+    const GoogleSignIn = async () => {
+        try {
+            await authClient.signIn.social({
+                provider: "google",
+                callbackURL: "/",
+            });
+            toast.success("Login successful!");
+            return;
+
+        } catch (error) {
+            console.error(error);
+            toast.error("Google login failed");
+        }
+    };
+
 
     const handleLogInSubmit = async (e) => {
         e.preventDefault();
@@ -16,52 +33,42 @@ const LogInPage = () => {
 
         // console.log("Form Data :", data);
 
-        
 
-         const { data, error } = await authClient.signIn.email({
+
+        const { data, error } = await authClient.signIn.email({
             email: userData?.email, // required
             password: userData?.password, // required
-           
+
         });
 
-        if(data){
-          router.push('/')
-        toast.success("Login successful!");
+        if (data) {
+            router.push('/')
+            toast.success("Login successful!");
         }
 
-        if(error){
+        if (error) {
             toast.error("Login failed. Please try again.");
-        
-                router.push('/login')
-            }
 
+            router.push('/login')
         }
 
-     const GoogleSignIn = async () => {
-       try {
-           await authClient.signIn.social({
-               provider: "google",
-               callbackURL: "/",
-           });
-   
-       } catch (error) {
-           console.error(error);
-           toast.error("Google login failed");
-       }
-   };
+    }
+
 
 
 
     return (
         <div className="flex items-center justify-center h-screen">
 
-            <form onSubmit={handleLogInSubmit}>
-                
-                <div className="card bg-base-100 max-w-full shadow-sm p-18 rounded-2xl">
 
-                    <h1 className="text-2xl font-bold mb-6 text-center text-cyan-100">
-                        Log In to Your Account
-                    </h1>
+
+            <div className="card bg-base-100 max-w-full shadow-sm p-18 rounded-2xl">
+
+                <h1 className="text-2xl font-bold mb-6 text-center text-cyan-100">
+                    Log In to Your Account
+                </h1>
+
+                <form onSubmit={handleLogInSubmit}>
 
                     <div className="mb-4">
                         <label>Email</label>
@@ -85,7 +92,7 @@ const LogInPage = () => {
                         />
                     </div>
 
-                    
+
 
                     <button
                         type="submit"
@@ -94,29 +101,31 @@ const LogInPage = () => {
                         Log In
                     </button>
 
-                    <div className="divider">OR</div>
 
-                    <div className="flex items-center justify-center mt-4">
+                </form>
+                <div className="divider">OR</div>
+
+                <div className="flex items-center justify-center mt-4">
                     <button onClick={GoogleSignIn} className="btn bg-white text-black border-[#e5e5e5]">
-                <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                Login with Google
-            </button>
+                        <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                        Login with Google
+                    </button>
                 </div>
 
 
-                 <div className="divider">OR</div>
+                <div className="divider">OR</div>
 
-                 <div>
+                <div>
                     <Link href="/register" className="text-sm text-cyan-100 hover:underline">
                         Don't have an account? Register here.
                     </Link>
-                 </div>
-
                 </div>
 
-                
+            </div>
 
-            </form>
+
+
+
 
         </div>
     );
